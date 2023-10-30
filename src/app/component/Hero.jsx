4f1634a54a18132a8a6fe6dc0b16dfdc02bb3afa
nandhayoyo <nandha.yuana2@Gmail.com>
2023-10-30 20:05:48 +0700

    feat : hero section fix image location

--- src/app/component/Hero.jsx
+++ src/app/component/Hero.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import verif_ic from "../../../public/Images/verif_ic.png";
+import girl_img from "../../../public/Images/Mask group.png";
+import barcode_logo from "../../../public/Images/ic_barcode.png";
+import key_logo from "../../../public/Images/ic_key.png";
+import verif_logo from "../../../public/Images/ic_verif.png";
+
 import Image from "next/image";
 
 const Hero = () => {
   return (
-    <section className="mt-20 bg-white dark:bg-gray-900">
+    <section className="bg-image bg-center bg-no-repeat bg-cover dark:bg-gray-900">
       <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div className="mr-auto place-self-center lg:col-span-6">
           <h1 className="max-w-2xl mb-4 text-4xl font-normal tracking-tight leading-none md:text-2xl xl:text-3xl text-blue-500 dark:text-white">
@@ -28,10 +33,51 @@ const Hero = () => {
             Hubungi Kami?
           </a>
         </div>
-        <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
-          <img
-            src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png"
+        <div className="hidden lg:mt-0 lg:col-span-5 lg:flex relative">
+          <Image
+            src={girl_img}
             alt="mockup"
+            style={{
+              position: "relative",
+              bottom: "63px",
+              left: "239px",
+            }}
+          />
+        </div>{" "}
+        <div className="hidden lg:col-span-5 lg:flex">
+          <Image
+            src={barcode_logo}
+            alt="mockup"
+            style={{
+              width: "150px",
+              position: "relative",
+              bottom: "206px",
+              left: "1230px",
+            }}
+          />
+        </div>
+        <div className="hidden lg:col-span-5 lg:flex">
+          <Image
+            src={key_logo}
+            alt="mockup"
+            style={{
+              width: "150px",
+              position: "relative",
+              bottom: "485px",
+              left: "100px",
+            }}
+          />
+        </div>
+        <div className="hidden lg:col-span-5 lg:flex">
+          <Image
+            src={verif_logo}
+            alt="mockup"
+            style={{
+              width: "150px",
+              position: "relative",
+              bottom: "250px",
+              left: "860px",
+            }}
           />
         </div>
       </div>
